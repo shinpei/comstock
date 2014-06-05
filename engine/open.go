8f660e6430e26b92289f45e61d266dff3f5f7c7e
shinpei <shinpei.nakata@gmail.com>
2014-06-06 07:19:30 +0900

    fix #6

--- engine/open.go
+++ engine/open.go
@@ -5,11 +5,20 @@ import (
 	"strings"
 )
 
+// codes from link below
+// http://stackoverflow.com/a/14053693/3070610
 func (e *Engine) Open(URL string) (err error) {
+	var browserCommand string = ""
+
 	if strings.Contains(e.env.OS, "darwin") {
-		cmd := exec.Command("open", URL)
-		err = cmd.Start()
+		browserCommand = "open"
+	} else if strings.Contains(e.env.OS, "linux") || strings.Contains(e.env.OS, "bsd") {
+		browserCommand = "xdg-open"
+	} else if strings.Contains(e.env.OS, "mingw") {
+		browserCommand = "start"
 	}
+	cmd := exec.Command(browserCommand, URL)
+	err = cmd.Start()
 
 	return
 }
