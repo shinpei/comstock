07ab5f806c15f88ba67e2288e92e199accbce02b
shinpei <shinpei.nakata@gmail.com>
2014-07-23 01:11:40 +0900

    add shell scripts

---
+++ dist/save_previous.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Comstock - stock your command to the cloud
+# comstock shell scripts
+# shinpei(c)2014
+
+saveBashHistory() {
+    fc -ln -2 > hoge
+    cat hoge | comstock save
+}
+
+# entry point
+if [ -z "$BASH_VERSION" ] || [ "${BASH##*/}" != "bash" ]; then 
+    saveBashHistory
+fi
+
