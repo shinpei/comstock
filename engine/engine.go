5b30b2479407bc172d73e2a0b2734e7fdb5579e7
shinpei <shinpei.nakata@gmail.com>
2014-08-24 10:49:01 +0900

    update fetch command

--- engine/engine.go
+++ engine/engine.go
@@ -292,7 +292,9 @@ func (e *Engine) Config() {
 }
 
 func (e *Engine) FetchCommandFromNumber(num int) (cmd *model.Command, err error) {
-
+	if e.storager.IsRequireLogin() == true && e.isLogin == false {
+		log.Fatal("You have no valid access token. Please login first.")
+	}
 	cmd, err = e.storager.FetchCommandFromNumber(e.userinfo, num)
 	if err == model.ErrSessionExpires {
 		e.SetLogout()
