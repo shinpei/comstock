a154a7c738ed700f17cec71db80f80ec3a6dfc35
shinpei <shinpei.nakata@gmail.com>
2014-08-24 10:31:35 +0900

    add ErrCommandNotFound

--- model/error.go
+++ model/error.go
@@ -20,5 +20,8 @@ var ErrIncorrectPassword = errors.New("Password is incorrect")
 var ErrAuthenticationFailed = errors.New("Authentication failed")
 var ErrAlreadyLogin = errors.New("User already logged in")
 
+// fetch
+var ErrCommandNotFound = errors.New("Requested Command not found")
+
 // other error
 var ErrServerSystem = errors.New("Comstock server has internal error")
