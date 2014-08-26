3fa0e582d10d61d45aeb8509f085c9ba1ae8dfad
shinpei <shinpei.nakata@gmail.com>
2014-08-27 01:51:59 +0900

    for version 0.1.4

--- build/compile.sh
+++ build/compile.sh
@@ -5,11 +5,11 @@ set -e
 DIR=$(cd $(dirname ${0})/.. && pwd)
 cd ${DIR}
 
-XC_ARCH=${XC_ARCH:-386 amd64}
+XC_ARCH=${XC_ARCH:-amd64}
 XC_OS=${XC_OS:-darwin linux}
 
 rm -rf pkg/
 gox \
     -os="${XC_OS}" \
     -arch="${XC_ARCH}" \
-    -output "pkg/{{.OS}}_{{.Arch}}/{{.Dir}}"
+    -output "pkg/{{.OS}}_{{.Arch}}/{{.Dir}}-cli"
