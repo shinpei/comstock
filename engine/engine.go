56ba594ce505d418254afbe5acfa11f03a264d4f
shinpei <shinpei.nakata@gmail.com>
2014-09-01 00:42:59 +0900

    separate fetch.go

--- engine/engine.go
+++ engine/engine.go
@@ -325,14 +325,3 @@ func (e *Engine) Close() {
 func (e *Engine) Config() {
 	e.config.ShowConfig()
 }
-
-func (e *Engine) FetchCommandFromNumber(num int) (cmd *model.Command, err error) {
-	if e.storager.IsRequireLogin() == true && e.isLogin == false {
-		log.Fatal("You have no valid access token. Please login first.")
-	}
-	cmd, err = e.storager.FetchCommandFromNumber(e.userinfo, num)
-	if err == model.ErrSessionExpires || err == model.ErrSessionInvalid {
-		e.SetLogout()
-	}
-	return
-}
