6a2ef6b081e34383b00c40f0ff410eabe3a69fc5
shinpei <shinpei.nakata@gmail.com>
2014-09-03 00:05:47 +0900

    err handling for open command

--- engine/open.go
+++ engine/open.go
@@ -7,23 +7,26 @@ import (
 )
 
 func OpenAction(c *cli.Context) {
-	eng.Open(eng.apiServer)
+	err := eng.Open(eng.apiServer)
+	if err != nil {
+		fmt.Printf("Seems it's failed to execute browser, visit https://comstock.herokuapp.com directly")
+	}
 }
 
 // codes from link below
 // http://stackoverflow.com/a/14053693/3070610
 func (e *Engine) Open(URL string) (err error) {
-	var browserCommand string = ""
+	browserCommand := ""
 
 	if strings.Contains(e.env.OS, "darwin") {
 		browserCommand = "open"
 	} else if strings.Contains(e.env.OS, "linux") || strings.Contains(e.env.OS, "bsd") {
 		browserCommand = "xdg-open"
 	} else if strings.Contains(e.env.OS, "mingw") {
+		// couldn't be?
 		browserCommand = "start"
 	}
 	cmd := exec.Command(browserCommand, URL)
 	err = cmd.Start()
-
 	return
 }
