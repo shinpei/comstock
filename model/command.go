47e5eca30844fee455088c6ab2cbf69d90765177
shinpei <shinpei.nakata@gmail.com>
2014-09-11 22:15:56 +0900

    hashing added

--- model/command.go
+++ model/command.go
@@ -1,16 +1,17 @@
 package model
 
 import (
+	"crypto/md5"
 	"time"
 )
 
 type Command struct {
 	Cmd       string
 	Timestamp string
+	Hash      []byte
 }
 
 func CreateCommand(cmd string) *Command {
-
-	return &Command{Cmd: cmd, Timestamp: time.Now().Format(time.RFC3339)}
-
+	h := md5.New()
+	return &Command{Cmd: cmd, Timestamp: time.Now().Format(time.RFC3339), Hash: h.Sum([]byte(cmd))}
 }
