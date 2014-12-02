5795789db6e8f2fbcafb84875525f4325d05523d
shinpei <shinpei.nakata@gmail.com>
2014-12-02 21:00:22 +0900

    update install sh

--- dist/install.sh
+++ dist/install.sh
@@ -1,7 +1,7 @@
 set -e
 
 COMSTOCK_GITHUB_RELEASE_URL=https://github.com/shinpei/comstock/releases/download
-COMSTOCK_VERSION=0.1.9
+COMSTOCK_VERSION=0.2.0-pre
 COMSTOCK_ARCH=amd64
 
 if [ `uname` = "Darwin" ]; then
